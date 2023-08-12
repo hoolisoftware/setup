8cb223a4304cd40984fc579c46bd15a184900e6a
Ilkhan <ilhanchanel20002@gmail.com>
2023-08-12 20:56:05 +0300

    .

--- stack-drf-nextjs.bat
+++ stack-drf-nextjs.bat
@@ -1,27 +1,30 @@
 @echo off
 
-mkdir stack-drf-nextjs
+if exist stack-drf-nextjs rmdir /S stack-drf-nextjs
 
+mkdir stack-drf-nextjs
 cd stack-drf-nextjs
 
-virtualenv .venv
+@REM server setup
 
-call .venv/Scripts/activate
+mkdir server && cd server
 
+virtualenv .venv && call .venv/Scripts/activate
 pip install django==4.2 pillow==10.0.0 django-rest-framework==0.1.0
+wget https://raw.githubusercontent.com/hoolisoftware/setup/master/python-django-drf.gitignore -O .gitignore
 
 django-admin startproject core
-rename core server
-
-wget https://raw.githubusercontent.com/hoolisoftware/setup/master/python-django-drf.gitignore -O .gitignore
+rename core src
+cd src
+python manage.py migrate
+python manage.py createsuperuser --no-input --username admin --email admin@admin.com
+echo DJANGO_SUPERUSER_PASSWORD='admin' >> core/settings.py
+cd ..
 
 cd ..
 
-mkdir client
+@REM client setup
 
-cd client
+npx create-next-app@13.4.12 client --use-yarn --ts --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
 
-npx create-next-app@13.4.12 . --use-yarn --ts --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
-
-..
-
+wget https://raw.githubusercontent.com/hoolisoftware/setup/master/stack-drf-nextjs.code-workspace -O vscode.code-workspace 
